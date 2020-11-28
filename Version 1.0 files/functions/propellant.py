501b2fa9ee9b59c9345968b56d4c5f5c3100814f
felipebogaertsm <felipebogaerts@gmail.com>
2020-11-28 15:54:23 -0300

    Version 2.0

---
+++ Version 1.0 files/functions/propellant.py
@@ -0,0 +1,128 @@
+# Burn rate data was gathered from Nakka and Magnus Gudnason paper 's072205'.
+# Propellant data was gathered from ProPEP3.
+#
+# ce: Combustion, two phase, heat loss, friction inefficiency factor
+# pp: Propellant density [kg/m^3]
+# k_mix_ch: Isentropic exponent (chamber)
+# k_2ph_ex: Isentropic exponent (exhaust)
+# T0_ideal: Ideal combustion temperature [K]
+# T0: Real combustion temperature [K]
+# M_ch: Molar weight (chamber) [100g/mole]
+# M_ex: Molar weight (exhaust) [100g/mole]
+# Isp_frozen, Isp_shifting: Frozen and shifting specific getImpulses [s]
+# qsi_ch: Number of condensed phase moles per 100 gram (chamber) [mole]
+# qsi_ex: Number of condensed phase moles per 100 gram (exhaust) [mole]
+
+
+class Propellant:
+
+    def __init__(self, ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex):
+        self.ce = ce
+        self.pp = pp
+        self.k_mix_ch = k_mix_ch
+        self.k_2ph_ex = k_2ph_ex
+        self.T0_ideal = T0_ideal
+        self.M_ch = M_ch
+        self.M_ex = M_ex
+        self.Isp_frozen = Isp_frozen
+        self.Isp_shifting = Isp_shifting
+        self.qsi_ch = qsi_ch
+        self.qsi_ex = qsi_ex
+
+
+# PROPELLANTS:
+
+kndx = Propellant(0.95, 1795.0 * 1.00, 1.1308, 1.0430, 1712, 42.391 * 1e-3,
+                  42.882 * 1e-3, 152.4, 154.1, 0.307, 0.321)
+knsb = Propellant(0.95, 1837.3 * 0.95, 1.1361, 1.0420, 1603, 39.857 * 1e-3,
+                  40.048 * 1e-3, 151.4, 153.5, 0.316, 0.321)
+knsu = Propellant(0.95, 1899.5 * 0.95, 1.1330, 1.1044, 1722, 41.964 * 1e-3,
+                  41.517 * 1e-3, 153.3, 155.1, 0.306, 0.321)
+kner = Propellant(0.94, 1820.0 * 0.95, 1.1390, 1.0426, 1608, 38.570 * 1e-3,
+                  38.779 * 1e-3, 153.8, 156.0, 0.315, 0.321)
+
+
+def prop_data(prop: str):
+
+    """"
+    Returns propellant data based on the propellant name entered by the user as a string.
+    """
+
+    if prop.lower() == 'kndx':
+        ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex = \
+            kndx.ce, kndx.pp, kndx.k_mix_ch, kndx.k_2ph_ex, kndx.T0_ideal, kndx.M_ch, kndx.M_ex, kndx.Isp_frozen, \
+            kndx.Isp_shifting, kndx.qsi_ch, kndx.qsi_ex
+    elif prop.lower() == 'knsb' or prop.lower() == 'knsb-nakka':
+        ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex = \
+            knsb.ce, knsb.pp, knsb.k_mix_ch, knsb.k_2ph_ex, knsb.T0_ideal, knsb.M_ch, knsb.M_ex, knsb.Isp_frozen, \
+            knsb.Isp_shifting, knsb.qsi_ch, knsb.qsi_ex
+    elif prop.lower() == 'knsu':
+        ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex = \
+            knsu.ce, knsu.pp, knsu.k_mix_ch, knsu.k_2ph_ex, knsu.T0_ideal, knsu.M_ch, knsu.M_ex, knsu.Isp_frozen, \
+            knsu.Isp_shifting, knsu.qsi_ch, knsu.qsi_ex
+    elif prop.lower() == 'kner':
+        ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex = \
+            kner.ce, kner.pp, kner.k_mix_ch, kner.k_2ph_ex, kner.T0_ideal, kner.M_ch, kner.M_ex, kner.Isp_frozen, \
+            kner.Isp_shifting, kner.qsi_ch, kner.qsi_ex
+    else:
+        print('\nPropellant name not recognized. Using values for KNSB instead.')
+        ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex = \
+            knsb.ce, knsb.pp, knsb.k_mix_ch, knsb.k_2ph_ex, knsb.T0_ideal, knsb.M_ch, knsb.M_ex, knsb.Isp_frozen, \
+            knsb.Isp_shifting, knsb.qsi_ch, knsb.qsi_ex
+
+    return ce, pp, k_mix_ch, k_2ph_ex, T0_ideal, M_ch, M_ex, Isp_frozen, Isp_shifting, qsi_ch, qsi_ex
+
+
+def get_burn_rate_coefs(prop: str, P0: float):
+    """
+    Sets the burn rate coefficients 'a' and 'n' according to the instantaneous chamber pressure
+    """
+
+    if prop.lower() == 'kndx':
+        if P0 * 1e-6 <= 0.779:
+            a, n = 8.875, 0.619
+        elif 0.779 < P0 * 1e-6 <= 2.572:
+            a, n = 7.553, -0.009
+        elif 2.572 < P0 * 1e-6 <= 5.930:
+            a, n = 3.841, 0.688
+        elif 5.930 < P0 * 1e-6 <= 8.502:
+            a, n = 17.2, -0.148
+        elif 8.502 < P0 * 1e-6 <= 11.20:
+            a, n = 4.775, 0.442
+        else:
+            print('\nCHAMBER PRESSURE OUT OF BOUNDS, change Propellant or nozzle throat diameter.\n')
+            a, n = -1, -1
+    elif prop.lower() == 'knsb-nakka':
+        if P0 * 1e-6 <= 0.807:
+            a, n = 10.708, 0.625
+        elif 0.807 < P0 * 1e-6 <= 1.503:
+            a, n = 8.763, -0.314
+        elif 1.503 < P0 * 1e-6 <= 3.792:
+            a, n = 7.852, -0.013
+        elif 3.792 < P0 * 1e-6 <= 7.033:
+            a, n = 3.907, 0.535
+        elif 7.033 < P0 * 1e-6 <= 10.67:
+            a, n = 9.653, 0.064
+        else:
+            print('\nCHAMBER PRESSURE OUT OF BOUNDS, change Propellant or nozzle throat diameter.\n')
+            a, n = -1, -1
+    elif prop.lower() == 'knsb':
+        a, n = 5.132, 0.222
+    elif prop.lower() == 'knsu':
+        a, n = 8.260, 0.319
+    elif prop.lower() == 'kndxio':
+        a, n = 9.25, 0.342
+    elif prop.lower() == 'kndxch':
+        a, n = 11.784, 0.297
+    elif prop.lower() == 'rnx57':
+        a, n = 2.397, 0.446
+    elif prop.lower() == 'kner':
+        a, n = 2.903, 0.395
+    elif prop.lower() == 'knmn':
+        a, n = 5.126, 0.224
+    elif prop.lower() == 'custom':
+        a, n = input('Type value of "a": '), input('Type value of "n": ')
+    else:
+        a, n = input('Type value of "a": '), input('Type value of "n": ')
+
+    return a, n
---
+++ Version 1.0 files/functions/structural_functions.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MotorStructure:
+    def __init__(self, sf, m_motor, D_in, D_out, L_cc, D_screw, D_clearance):
+        self.sf = sf
+        self.m_motor = m_motor
+        self.D_in = D_in
+        self.D_out = D_out
+        self.L_cc = L_cc
+        self.D_screw = D_screw
+        self.D_clearance = D_clearance
+
+    def bulkhead_thickness(self, Y_bulkhead, P0):
+        """ Returns the thickness of a plane bulkhead for a pressure vessel """
+        bulkhead_t = self.D_in * np.sqrt((0.75 * np.max(P0)) / (Y_bulkhead / self.sf))
+        return bulkhead_t
+
+    def nozzle_thickness(self, Y_nozzle, Div_angle, Conv_angle, P0):
+        """ Returns nozzle convergent and divergent thickness """
+        nozzle_conv_t = (np.max(P0) * self.D_in / 2) / (Y_nozzle / self.sf - 0.6 * np.max(P0)
+                                                        * (np.cos(np.deg2rad(Conv_angle))))
+        nozzle_div_t = (np.max(P0) * self.D_in / 2) / (Y_nozzle / self.sf - 0.6 * np.max(P0)
+                                                       * (np.cos(np.deg2rad(Div_angle))))
+        return nozzle_conv_t, nozzle_div_t
+
+    def casing_safety_factor(self, Y_cc, P0):
+        """ Returns the thickness for a cylindrical pressure vessel """
+        thickness = (self.D_out - self.D_in) / 2
+        P_bursting = (Y_cc * thickness) / (self.D_in * 0.5 + 0.6 * thickness)
+        casing_sf = P_bursting / np.max(P0)
+        return casing_sf
+
+    def optimal_fasteners(self, max_number_of_screws, P0, Y_cc, U_screw):
+        shear_sf = np.zeros(max_number_of_screws)
+        tear_sf = np.zeros(max_number_of_screws)
+        compression_sf = np.zeros(max_number_of_screws)
+
+        for i in range(1, max_number_of_screws + 1):
+            Area_shear = (self.D_screw ** 2) * np.pi * 0.25
+            Area_tear = (((np.pi * 0.25 * ((self.D_out ** 2) - (self.D_in ** 2))) / i) -
+                         (np.arcsin((self.D_clearance / 2) / (self.D_in / 2))) * 0.25
+                         * ((self.D_out ** 2) - (self.D_in ** 2)))
+            Area_compression = ((self.D_out / 2) - (self.D_in / 2)) * self.D_clearance
+            forceFastener = (np.max(P0) * (np.pi * (self.D_in / 2) ** 2)) / i
+            shear_stress = forceFastener / Area_shear
+            shear_sf[i - 1] = U_screw / shear_stress
+            tear_stress = forceFastener / Area_tear
+            tear_sf[i - 1] = (Y_cc / np.sqrt(3)) / tear_stress
+            compression_stress = forceFastener / Area_compression
+            compression_sf[i - 1] = Y_cc / compression_stress
+
+        sfFastener = np.vstack((shear_sf, tear_sf, compression_sf))
+        max_sf_fastener = np.max(np.min(sfFastener, axis=0))
+        optimal_fasteners = np.argmax(np.min(sfFastener, axis=0))
+        return optimal_fasteners, max_sf_fastener, shear_sf, tear_sf, compression_sf
