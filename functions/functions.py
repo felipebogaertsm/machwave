ce766aff65b0c58b73f7d7cdf33593fdebd84f94
felipebogaertsm <felipe.bogaerts@engenharia.ufjf.br>
2020-12-28 19:15:00 -0300

    Version 2.0

--- functions/functions.py
+++ functions/functions.py
@@ -3,6 +3,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.subplots
+
+
+def performance_interactive_plot(t, P0, T, t_thrust):
+
+    index = np.where(t == t_thrust)[0][0]
+
+    figure = plotly.subplots.make_subplots(specs=[[{"secondary_y": True}]])
+
+    figure.add_trace(go.Scatter(x=t[: index], y=T[: index], mode='lines', name='Thrust'), secondary_y=False)
+    figure.add_trace(go.Scatter(x=t[: index], y=P0[: index] * 1e-6, name='Pressure'), secondary_y=True)
+
+    figure.update_layout(
+        title_text='<b>Performance plot<b>'
+    )
+
+    # Set x-axis title
+    figure.update_xaxes(title_text='Time (s)')
+
+    # Set y-axes titles
+    figure.update_yaxes(title_text="<b>Thrust</b> (N)", secondary_y=False)
+    figure.update_yaxes(title_text="<b>Pressure</b> (MPa)", secondary_y=True)
+
+    figure.show()
+
+    pass
 
 
 def motor_to_eng(t, F, dt, V_prop_CP, D_out, L_chamber, eng_res, pp, m_motor, manufacturer, name):
