9f8336f26fe990e8ec11a1b086806e6ca281f698
felip <felipebogaerts@gmail.com>
2021-03-14 12:08:04 -0300

    Version 2.2
    SRM Solver web app project started!

---
+++ srm_solver_web/srm_solver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SrmSolverConfig(AppConfig):
+    name = 'srm_solver'
---
+++ srm_solver_web/srm_solver/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# redirect function redirects the user to another URL after log in has been successful.
+from django.shortcuts import render, redirect
+# Importa o formulário de criação e autenticação de usuário:
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+# Imports the decorator for the login required views:
+from django.contrib.auth.decorators import login_required
+# Imports functions from django that log in, log out and authenticate users
+from django.contrib.auth import login, logout, authenticate
+# Importing HTTP responses and File response (the last one for downloading files):
+from django.http import HttpResponse, FileResponse
+import pathlib
+import os
+
+# from .forms import *
+from .models import *
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'srm_solver/home.html')
+
+
+def about(request):
+    return render(request, 'srm_solver/about.html')
+
+
+def loginuser(request):
+    """
+    Logs in the user and redirects to the automation home page.
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        # In case the user has just entered the page:
+        return render(request, 'srm_solver/loginuser.html', {'log_form': AuthenticationForm()})
+    else:  # in case the user has already posted the request
+        # Checking if the user credentials are correct:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:  # in case the user does not exist, seng him back to log in page with an error
+            return render(request, 'srm_solver/loginuser.html', {'form': AuthenticationForm(),
+                                                                 'error': 'Username and password do not exist'})
+        else:  # in case the user has been successfully authenticated
+            # Redirects the user to the main automation page after log in has been concluded successfully.
+            login(request, user)
+            return redirect('home')
+
+
+def logoutuser(request):
+    # Method is 'POST' since some browsers will not work with 'GET'. Some browsers automatically load 'GET' requests.
+    # This automatic loading of some items would log out the user as soon as the page loads.
+    if request.method == 'POST':
+        logout(request)
+        # In case the user logs out, he is redirected to the home page.
+        return redirect('home')
