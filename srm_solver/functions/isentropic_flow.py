e5c21546a838c4c490dc7b37c0918eb141d14ce0
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-16 22:53:33 -0300

    0.3.0
    - Organizing the files

--- srm_solver/functions/isentropic_flow.py
+++ srm_solver/functions/isentropic_flow.py
@@ -22,44 +22,61 @@ def get_critical_pressure(k_mix_ch):
     return (2 / (k_mix_ch + 1)) ** (k_mix_ch / (k_mix_ch - 1))
 
 
-def get_opt_expansion_ratio(k, P0, P_ext):
+def get_opt_expansion_ratio(k, P_0, P_ext):
     """
     Returns the optimal expansion ratio based on the current chamber pressure,
     specific heat ratio and external pressure.
     """
-    Exp_opt = ((((k + 1) / 2) ** (1 / (k - 1))) * ((P_ext / P0) ** (1 / k)) *
-               np.sqrt(((k + 1) / (k - 1)) * (1 - (P_ext / P0) ** ((k - 1) / k)))) ** - 1
-    return Exp_opt
+    exp_opt = (
+        (((k + 1) / 2) ** (1 / (k - 1))) *
+        ((P_ext / P_0) ** (1 / k)) *
+        np.sqrt(
+            ((k + 1) / (k - 1)) * (1 - (P_ext / P_0) ** ((k - 1) / k))
+        )
+    ) ** - 1
+
+    return exp_opt
 
 
 def get_exit_mach(k: float, E: float):
     """
     Gets the exit Mach number of the nozzle flow.
     """
-    M_exit = scipy.optimize.fsolve(
-        lambda x: (((1 + 0.5 * (k - 1) * x ** 2) / (1 + 0.5 * (k - 1))) ** ((k + 1) / (2 * (k - 1)))) / x - E,
+    exit_mach_no = scipy.optimize.fsolve(
+        lambda x: (
+            (
+                (1 + 0.5 * (k - 1) * x ** 2) / (1 + 0.5 * (k - 1))
+            ) ** (
+                (k + 1) / (2 * (k - 1))
+            )
+        ) / x - E,
         [10]
     )
-    return M_exit[0]
+    return exit_mach_no[0]
 
 
-def get_exit_pressure(k_2ph_ex, E, P0):
+def get_exit_pressure(k_2ph_ex, E, P_0):
     """
     Returns the exit pressure of the nozzle flow.
     """
     Mach_exit = get_exit_mach(k_2ph_ex, E)
-    P_exit = P0 * (1 + 0.5 * (k_2ph_ex - 1) * Mach_exit ** 2) ** (- k_2ph_ex / (k_2ph_ex - 1))
+    P_exit = P_0 * (1 + 0.5 * (k_2ph_ex - 1) * Mach_exit ** 2) ** (
+        - k_2ph_ex / (k_2ph_ex - 1)
+    )
     return P_exit
 
 
-def get_thrust_coeff(P0, P_exit, P_external, E, k, n_cf):
+def get_thrust_coeff(P_0, P_exit, P_external, E, k, n_cf):
     """
     Returns value for thrust coefficient based on the chamber pressure and
     correction factor.
     """
-    P_r = P_exit / P0
-    Cf_ideal = np.sqrt((2 * (k ** 2) / (k - 1)) * ((2 / (k + 1)) ** ((k + 1) / (k - 1))) * (1 - (P_r ** ((k - 1) / k))))
-    Cf = (Cf_ideal + E * (P_exit - P_external) / P0) * n_cf
+    P_r = P_exit / P_0
+    Cf_ideal = np.sqrt(
+        (2 * (k ** 2) / (k - 1)) *
+        ((2 / (k + 1)) ** ((k + 1) / (k - 1))) * (1 - (P_r ** ((k - 1) / k)))
+    )
+    Cf = (Cf_ideal + E * (P_exit - P_external) / P_0) * n_cf
 
     if Cf <= 0:
         Cf = 0
@@ -76,16 +93,19 @@ def get_impulses(F_avg, t, t_burnout, m_prop):
     """
     t = t[t <= t_burnout]
     index = np.where(t == t_burnout)
+
     m_prop = m_prop[: index[0][0]]
+
     I_total = F_avg * t[-1]
     I_sp = I_total / (m_prop[0] * 9.81)
+
     return I_total, I_sp
 
 
 def get_operational_correction_factors(
-    P0,
+    P_0,
     P_external,
-    P0_psi,
+    P_0_psi,
     propellant,
     structure,
     critical_pressure_ratio,
@@ -95,33 +115,47 @@ def get_operational_correction_factors(
     """
     Returns kinetic, two-phase and boundary layer correction factors based
     on a015140.
+
+    :param P_0: chamber stagnation pressure (Pa)
+    :param P_external: external pressure
+    :param P_0_psi: chamber pressure in psi
+    :param propellant: propellant object
+    :param structure: Structure object
+    :param critical_pressure_ratio: critical pressure ratio
+    :param V0: free chamber volume
+    :param t: current time
     """
 
     # Kinetic losses
-    if P0_psi >= 200:
+    if P_0_psi >= 200:
         n_kin = 33.3 * 200 * (
             propellant.Isp_frozen / propellant.Isp_shifting
-        ) / P0_psi
+        ) / P_0_psi
     else:
         n_kin = 0
 
     # Boundary layer and two phase flow losses
-    if P_external / P0 <= critical_pressure_ratio:
+    if P_external / P_0 <= critical_pressure_ratio:
 
         termC2 = 1 + 2 * np.exp(
-            - structure.C2 * P0_psi ** 0.8 * t / (
+            - structure.C2 * P_0_psi ** 0.8 * t / (
                 (structure.nozzle_throat_diameter / 0.0254) ** 0.2
             )
         )
         E_cf = 1 + 0.016 * structure.expansion_ratio ** - 9
         n_bl = structure.C1 * (
-            (P0_psi ** 0.8) / (
+            (P_0_psi ** 0.8) / (
                 (structure.nozzle_throat_diameter / 0.0254) ** 0.2
             )
         ) * termC2 * E_cf
 
-        C7 = 0.454 * (P0_psi ** 0.33) * (propellant.qsi_ch ** 0.33) * (1 - np.exp(- 0.004 * (V0 / get_circle_area(
-            structure.nozzle_throat_diameter)) / 0.0254) * (1 + 0.045 * structure.nozzle_throat_diameter / 0.0254))
+        C7 = 0.454 * (P_0_psi ** 0.33) * (propellant.qsi_ch ** 0.33) * (
+            1 - np.exp(
+                - 0.004 * (V0 / get_circle_area(
+                    structure.nozzle_throat_diameter
+                )) / 0.0254
+            ) * (1 + 0.045 * structure.nozzle_throat_diameter / 0.0254)
+        )
         if 1 / propellant.M_ch >= 0.9:
             C4 = 0.5
             if structure.nozzle_throat_diameter / 0.0254 < 1:
@@ -148,8 +182,13 @@ def get_operational_correction_factors(
                     C3, C5, C6 = 30.4, 0.8, 0.4
                 elif C7 > 8:
                     C3, C5, C6 = 25.2, 0.8, 0.33
-        n_tp = C3 * ((propellant.qsi_ch * C4 * C7 ** C5) / (P0_psi ** 0.15 * structure.expansion_ratio ** 0.08 *
-                                                            (structure.nozzle_throat_diameter / 0.0254) ** C6))
+        n_tp = C3 * (
+            (propellant.qsi_ch * C4 * C7 ** C5) / (
+                P_0_psi ** 0.15 * structure.expansion_ratio ** 0.08 * (
+                    structure.nozzle_throat_diameter / 0.0254
+                    ) ** C6
+                )
+        )
     else:
         n_tp = 0
         n_bl = 0
@@ -158,21 +197,30 @@ def get_operational_correction_factors(
 
 
 def get_expansion_ratio(
-    Pe: float,
-    P0: list,
+    P_e: list,
+    P_0: list,
     k: float,
     critical_pressure_ratio: float
 ):
     """
     Returns array of the optimal expansion ratio for each pressure ratio.
-    """
-    E = np.zeros(np.size(P0))
 
-    for i in range(np.size(P0)):
-        if Pe / P0[i] <= critical_pressure_ratio:
-            pressure_ratio = Pe / P0[i]
-            E[i] = (((k + 1) / 2) ** (1 / (k - 1)) * pressure_ratio ** (1 / k) * (
-                    (k + 1) / (k - 1) * (1 - pressure_ratio ** ((k - 1) / k))) ** 0.5) ** -1
+    :param P_e: pressure
+    :param P_0: chamber stagnation pressure
+    :param k: isentropic exponent
+    :return: mean expansion ratio
+    """
+    E = np.zeros(np.size(P_0))
+
+    for i in range(np.size(P_0)):
+        if P_e / P_0[i] <= critical_pressure_ratio:
+            pressure_ratio = P_e / P_0[i]
+            E[i] = (
+                ((k + 1) / 2) ** (1 / (k - 1)) * pressure_ratio ** (1 / k) * (
+                    (k + 1) / (k - 1) * (
+                        1 - pressure_ratio ** ((k - 1) / k))
+                    ) ** 0.5
+                ) ** - 1
         else:
             E[i] = 1
     return np.mean(E)
