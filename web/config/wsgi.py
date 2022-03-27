55c72771c13b9418435607ad1a25225439b0f9d0
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-03-27 10:08:43 -0300

    0.3.23
    Rellocated files

--- web/config/wsgi.py
+++ web/config/wsgi.py
@@ -6,18 +6,18 @@
 # the Free Software Foundation, version 3.
 
 """
-ASGI config for srm_solver_app project.
+WSGI config for srm_solver_app project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "srm_solver_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+application = get_wsgi_application()
