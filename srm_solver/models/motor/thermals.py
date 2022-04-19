9871185e60d37563f7b676ba2ed8835093ce7a96
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-19 14:21:20 -0300

    0.3.37
    Classes for thermals and structural components

---
+++ srm_solver/models/motor/thermals.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Author: Felipe Bogaerts de Mattos
+# Contact me at felipe.bogaerts@engenharia.ufjf.br.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+
+from models.materials import Material
+
+
+class ThermalLiner:
+    def __init__(self, thickness: float, material: Material) -> None:
+        self.thickness = thickness
+        self.material = material
