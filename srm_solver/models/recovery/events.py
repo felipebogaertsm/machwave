7c93773ad8d5f6579e01d87f30379342ae6b0fc5
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-19 11:32:30 -0300

    0.3.33
    Recovery events

--- srm_solver/models/recovery/events.py
+++ srm_solver/models/recovery/events.py
@@ -5,24 +5,43 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+import numpy as np
+
+from models.recovery.parachutes import Parachute
+
 
 class RecoveryEvent:
-    def __init__(
-        self,
-        trigger_type,
-        trigger_value,
-    ) -> None:
-        self.trigger_type = trigger_type
+    def __init__(self, trigger_value, parachute: Parachute) -> None:
         self.trigger_value = trigger_value
-
-
-class ParachuteEvent(RecoveryEvent):
-    def __init__(
-        self,
-        trigger_type,
-        trigger_value,
-        parachute,
-    ) -> None:
-        super().__init__(trigger_type, trigger_value)
-
         self.parachute = parachute
+
+    def is_active(self):
+        pass
+
+
+class AltitudeBasedEvent(RecoveryEvent):
+    def is_active(self, height: float, velocity: float) -> bool:
+        if velocity < 0 and height < self.trigger_value:
+            return True
+        else:
+            return False
+
+
+class ApogeeBasedEvent(RecoveryEvent):
+    def is_active(
+        self,
+        height: np.array,
+        time: np.array,
+        propellant_mass: float,
+        time_after_apogee: float,
+    ):
+        max_height_index = np.argmax(height)
+        apogee_time = time[max_height_index]
+
+        if (
+            propellant_mass == 0
+            and time[-1] >= time_after_apogee + apogee_time
+        ):
+            return True
+
+        return False
