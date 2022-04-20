733e0d6ec62ac1242a1326aa5f7622a4b02537f8
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-19 21:19:39 -0300

    0.3.38
    - New materials
    - Main script updates

--- srm_solver/models/materials/elastics.py
+++ srm_solver/models/materials/elastics.py
@@ -8,15 +8,15 @@
 from . import Material
 
 
-class Steel(Material):
+class EPDM(Material):
     """
     Data obtained from:
-    https://www.thyssenkrupp-materials.co.uk/stainless-steel-304-14301.html
+    https://www.matweb.com/search/datasheet.aspx?matguid=f8e3355cc2c541fbb0174960466819c0&ckck=1
     """
 
     def __init__(self) -> None:
         super().__init__(
-            density=8000,
-            yield_strength=210e6,
-            ultimate_strength=520e6,
+            density=1500,
+            yield_strength=None,
+            ultimate_strength=17e6,
         )
