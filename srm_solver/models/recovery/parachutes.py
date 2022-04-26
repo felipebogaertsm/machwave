ecd6a3fac3092f1099a4bd8837c1b75595931ddd
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-26 14:27:39 -0300

    0.3.46
    Dependency inversion complete

--- srm_solver/models/recovery/parachutes.py
+++ srm_solver/models/recovery/parachutes.py
@@ -11,19 +11,22 @@ class can be implemented in different ways by inheriting from it and then
 changing its methods depending on the particular parachute geometry. 
 """
 
+from abc import ABC, abstractmethod
 
 from utils.geometric import get_circle_area
 
 
-class Parachute:
+class Parachute(ABC):
     def __init__(self) -> None:
         pass
 
     @property
+    @abstractmethod
     def drag_coefficient(self):
         pass
 
     @property
+    @abstractmethod
     def area(self):
         pass
 
