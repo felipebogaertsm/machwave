42771a3e5c0f8a9e0b807bb5a42fe455816fc264
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-26 22:36:48 -0300

    0.3.51

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,18 @@
+// Components:
+import { AppBar, Container, Toolbar, Typography } from "@mui/material"
+
 export default function Home() {
-    return <div>Hello</div>
+    return (
+        <div>
+            <div>
+                <AppBar position="static">
+                    <Container>
+                        <Toolbar>
+                            <Typography variant="h6">SRM SOLVER</Typography>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </div>
+        </div>
+    )
 }
