2205a707752735cbbcecd93b2cb35b3973e2b876
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-26 22:06:06 -0300

    0.3.48
    Removed old frontend

--- frontend/components/Heading.js
+++
@@ -1,7 +0,0 @@
-export default function Heading({ children }) {
-    return (
-        <div className='text-5xl font-bold'>
-            <h2>{children}</h2>
-        </div>
-    )
-}
--- frontend/components/Subheading.js
+++
@@ -1,9 +0,0 @@
-export default function Subheading(props) {
-    return (
-        <div {...props}>
-            <div className='text-3xl font-bold'>
-                <h2>{props.children}</h2>
-            </div>
-        </div>
-    )
-}
