89e7cdff5a3810257b0b5c9905dccc8c6e522058
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-26 22:02:38 -0300

    0.3.47
    Strategy pattern for atmospheric model

--- srm_solver/models/atmosphere/__init__.py
+++ srm_solver/models/atmosphere/__init__.py
@@ -5,13 +5,26 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+from abc import ABC, abstractmethod
+
 from fluids.atmosphere import ATMOSPHERE_1976
 
 
-class Atmosphere1976:
-    def __init__(self) -> None:
+class Atmosphere(ABC):
+    @abstractmethod
+    def get_density(self, y_amsl: float) -> float:
         pass
 
+    @abstractmethod
+    def get_gravity(self, y_amsl: float) -> float:
+        pass
+
+    @abstractmethod
+    def get_pressure(self, y_amsl: float) -> float:
+        pass
+
+
+class Atmosphere1976(Atmosphere):
     def get_density(self, y_amsl: float) -> float:
         """
         Gets the air density using the AMSL elevation and the fluids library.
