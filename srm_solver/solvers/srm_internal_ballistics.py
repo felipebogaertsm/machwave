2917f2f5b9623521a6509ecf20d6d6887ce5099f
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-03 13:55:22 -0300

    0.3.58
    Solver classes implemented

--- srm_solver/solvers/srm_internal_ballistics.py
+++ srm_solver/solvers/srm_internal_ballistics.py
@@ -5,10 +5,10 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
-from typing import Any, Callable, List
+from typing import List
 
 from solvers import Solver
-from utils.odes import ballistics_ode
+from utils.odes import solve_cp_seidel
 
 
 class SRMInternalBallisticsSolver(Solver):
@@ -23,5 +23,61 @@ class SRMInternalBallisticsSolver(Solver):
         k_mix_ch: float,
         R_ch: float,
         T_O: float,
+        burn_rate: float,
+        d_t: float,
     ) -> List[float]:
-        pass
+        k_1 = solve_cp_seidel(
+            chamber_pressure,
+            external_pressure,
+            burn_area,
+            propellant_volume,
+            nozzle_area,
+            propellant_density,
+            k_mix_ch,
+            R_ch,
+            T_O,
+            burn_rate,
+        )
+
+        k_2 = solve_cp_seidel(
+            chamber_pressure + 0.5 * k_1 * d_t,
+            external_pressure,
+            burn_area,
+            propellant_volume,
+            nozzle_area,
+            propellant_density,
+            k_mix_ch,
+            R_ch,
+            T_O,
+            burn_rate,
+        )
+
+        k_3 = solve_cp_seidel(
+            chamber_pressure + 0.5 * k_2 * d_t,
+            external_pressure,
+            burn_area,
+            propellant_volume,
+            nozzle_area,
+            propellant_density,
+            k_mix_ch,
+            R_ch,
+            T_O,
+            burn_rate,
+        )
+
+        k_4 = solve_cp_seidel(
+            chamber_pressure + 0.5 * k_3 * d_t,
+            external_pressure,
+            burn_area,
+            propellant_volume,
+            nozzle_area,
+            propellant_density,
+            k_mix_ch,
+            R_ch,
+            T_O,
+            burn_rate,
+        )
+
+        return (
+            chamber_pressure + (1 / 6) * (k_1 + 2 * (k_2 + k_3) + k_4) * d_t,
+        )
