91ecf74f61a1f07c39db137c7cdc6b1078a2b0d8
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-17 12:30:02 -0300

    0.3.65
    Fuselage data class

--- srm_solver/models/rocket/fuselage.py
+++ srm_solver/models/rocket/fuselage.py
@@ -5,29 +5,43 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+from dataclasses import dataclass, field
 
 from utils.geometric import get_circle_area
 
 
+class DragCoefficientTypeError(Exception):
+    def __init__(self, value: str, message: str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
+@dataclass
 class Fuselage:
     """
     Deals primarily with aerodynamic parameters.
     """
 
-    def __init__(
-        self,
-        length: float,
-        drag_coefficient: float,
-        outer_diameter: float,
-        frontal_area: float = None,
-    ) -> None:
-        self.length = length
-        self.drag_coefficient = drag_coefficient
-        self.outer_diameter = outer_diameter
-        self.frontal_area = self.get_frontal_area(frontal_area)
+    length: float | int
+    drag_coefficient: list[list[float, float]] | float | int
+    outer_diameter: float | int
+    frontal_area: float | None = field(default=None)
 
-    def get_frontal_area(self, frontal_area: float) -> float:
-        if frontal_area is not None:
-            return frontal_area
+    def __post_init__(self):
+        if self.frontal_area is None:
+            self.frontal_area = self.get_diameter_frontal_area()
+
+    def get_diameter_frontal_area(self) -> float:
+        return get_circle_area(self.outer_diameter)
+
+    def get_drag_coefficient(self, velocity: float = None):
+        if isinstance(self.drag_coefficient, list[list[float, float]]):
+            pass  # implement drag coefficient in function of velocity
+        elif isinstance(self.drag_coefficient, float | int):
+            return self.drag_coefficient
         else:
-            return get_circle_area(self.outer_diameter)
+            raise DragCoefficientTypeError(
+                self.drag_coefficient,
+                "Type not recognized in 'drag_coefficient'",
+            )
