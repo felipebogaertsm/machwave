ca53b7a075b7b9f6c16928701d3c8fc512ea77e6
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-25 20:50:41 -0300

    0.3.74
    Docs

--- srm_solver/models/motor/propellant.py
+++ srm_solver/models/motor/propellant.py
@@ -5,20 +5,6 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
-"""
-combustion_efficiency: Combustion, two phase, heat loss, friction inefficiency factor
-density: Propellant density [kg/m^3]
-k_mix_ch: Isentropic exponent (chamber)
-k_2ph_ex: Isentropic exponent (exhaust)
-T0_ideal: Ideal combustion temperature [K]
-T0: Real combustion temperature [K]
-M_ch: Molar weight (chamber) [100g/mole]
-M_ex: Molar weight (exhaust) [100g/mole]
-Isp_frozen, Isp_shifting: Frozen and shifting specific getImpulses [s]
-qsi_ch: Number of condensed phase moles per 100 gram (chamber) [mole]
-qsi_ex: Number of condensed phase moles per 100 gram (exhaust) [mole]
-"""
-
 import scipy.constants
 
 
@@ -48,7 +34,24 @@ class Propellant:
         qsi_ch: float,
         qsi_ex: float,
     ) -> None:
-        self._burn_rate = burn_rate
+        """
+        :param float burn_rate: Burn rate information list
+        :param float combustion_efficiency: Combustion, two phase,
+            heat loss, friction efficiency (0 to 1)
+        :param float density: Propellant density [kg/m^3]
+        :param float k_mix_ch: Isentropic exponent (chamber)
+        :param float k_2ph_ex: Isentropic exponent (exhaust)
+        :param float T0_ideal: Ideal combustion temperature [K]
+        :param float M_ch: Molar weight (chamber) [100g/mole]
+        :param float M_ex: Molar weight (exhaust) [100g/mole]
+        :param float Isp_frozen: Frozen specific impulse [s]
+        :param float Isp_shifting: Shipping specific impulse [s]
+        :param float qsi_ch: Number of condensed phase moles per 100 gram
+            (chamber) [moles]
+        :param float qsi_ex: Number of condensed phase moles per 100 gram
+            (exhaust) [moles]
+        """
+        self.burn_rate = burn_rate
         self.combustion_efficiency = combustion_efficiency
         self.density = density
         self.k_mix_ch = k_mix_ch
@@ -67,9 +70,11 @@ class Propellant:
 
     def get_burn_rate(self, chamber_pressure: float) -> float:
         """
-        chamber_pressure must be in Pascals.
+        :param float chamber_pressure: Instantaneous stagnation pressure [Pa]
+        :return: Instantaneous burn rate using St. Robert's law
+        :rtype: float
         """
-        for item in self._burn_rate:
+        for item in self.burn_rate:
             if item["min"] <= chamber_pressure <= item["max"]:
                 a = item["a"]
                 n = item["n"]
