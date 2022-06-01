860e43af7b0a0926dafee3e1275fae4812bd60c9
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-06-01 16:21:53 -0300

    0.3.85
    Test bolted combustion chamber

--- srm_solver/tests/test_models/test_propulsion/test_structure.py
+++ srm_solver/tests/test_models/test_propulsion/test_structure.py
@@ -6,9 +6,13 @@
 # the Free Software Foundation, version 3.
 
 
-def test_combustion_chamber_properties(combustion_chamber_olympus):
-    combustion_chamber = combustion_chamber_olympus
+def _test_combustion_chamber_properties(combustion_chamber):
+    """
+    Generic test function for CombustionChamber and its descendents.
 
+    Tests geometric properties of the class, such as inner radius
+    (calculated from inner diameter) and more.
+    """
     inner_diameter = combustion_chamber.inner_diameter
     assert inner_diameter > 0
     assert (
@@ -27,3 +31,13 @@ def test_combustion_chamber_properties(combustion_chamber_olympus):
         combustion_chamber.casing_inner_diameter
         == inner_diameter + 2 * combustion_chamber.liner.thickness
     )
+
+
+def test_combustion_chamber_properties(combustion_chamber_olympus):
+    _test_combustion_chamber_properties(combustion_chamber_olympus)
+
+
+def test_bolted_combustion_chamber_properties(
+    bolted_combustion_chamber_olympus,
+):
+    _test_combustion_chamber_properties(bolted_combustion_chamber_olympus)
