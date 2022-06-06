3b43888ada02bb26ed9a0b25ced1b15fffedfa08
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-06-06 10:33:52 -0300

    Incorporated structural sim

--- srm_solver/simulations/structural.py
+++ srm_solver/simulations/structural.py
@@ -9,28 +9,55 @@
 Stores structural simulation.
 """
 
+from dataclasses import dataclass
+
 import numpy as np
 
 from models.propulsion.structure import MotorStructure
+from operations.internal_ballistics import SRMOperation
 from simulations import Simulation
-from simulations.dataclasses.structural_parameters import StructuralParameters
+
+
+@dataclass
+class StructuralParameters:
+    def __init__(
+        self,
+        casing_sf,
+        nozzle_conv_t,
+        nozzle_div_t,
+        bulkhead_t,
+        optimal_fasteners,
+        max_sf_fastener,
+        shear_sf,
+        tear_sf,
+        compression_sf,
+    ):
+        self.casing_sf = casing_sf
+        self.nozzle_conv_t = nozzle_conv_t
+        self.nozzle_div_t = nozzle_div_t
+        self.bulkhead_t = bulkhead_t
+        self.optimal_fasteners = optimal_fasteners
+        self.max_sf_fastener = max_sf_fastener
+        self.shear_sf = shear_sf
+        self.tear_sf = tear_sf
+        self.compression_sf = compression_sf
 
 
 class StructuralSimulation(Simulation):
     def __init__(
         self,
         structure: MotorStructure,
-        chamber_pressure: np.array,
+        max_chamber_pressure: float,
         safety_factor: float,
     ) -> None:
         self.structure = structure
-        self.chamber_pressure = chamber_pressure
+        self.max_chamber_pressure = max_chamber_pressure
         self.safety_factor = safety_factor
 
     def run(self) -> StructuralParameters:
         # Casing thickness assuming thin wall [m]:
         casing_sf = self.structure.chamber.get_casing_safety_factor(
-            self.chamber_pressure
+            self.max_chamber_pressure
         )
 
         # Nozzle thickness assuming thin wall [m]:
@@ -38,12 +65,14 @@ class StructuralSimulation(Simulation):
             nozzle_conv_t,
             nozzle_div_t,
         ) = self.structure.nozzle.get_nozzle_thickness(
-            self.chamber_pressure, self.safety_factor, self.structure.chamber
+            self.max_chamber_pressure,
+            self.safety_factor,
+            self.structure.chamber,
         )
 
         # Bulkhead thickness [m]:
         bulkhead_t = self.structure.chamber.get_bulkhead_thickness(
-            self.chamber_pressure, self.safety_factor
+            self.max_chamber_pressure, self.safety_factor
         )
 
         # Screw safety factors and optimal quantity (shear, tear and compression):
@@ -54,10 +83,10 @@ class StructuralSimulation(Simulation):
             tear_sf,
             compression_sf,
         ) = self.structure.chamber.get_optimal_fasteners(
-            self.chamber_pressure,
+            self.max_chamber_pressure,
         )
 
-        return StructuralParameters(
+        self.structural_parameters = StructuralParameters(
             casing_sf,
             nozzle_conv_t,
             nozzle_div_t,
@@ -67,4 +96,32 @@ class StructuralSimulation(Simulation):
             shear_sf,
             tear_sf,
             compression_sf,
+        )
+
+        return self.structural_parameters
+
+    def print_results(self):
+        print("\nPRELIMINARY STRUCTURAL PROJECT")
+        print(
+            f" Casing safety factor: {self.structural_parameters.casing_sf:.2f}"
+        )
+        print(
+            f" Minimal nozzle convergent, divergent thickness: {self.structural_parameters.nozzle_conv_t * 1e3:.3f}, "
+            f"{self.structural_parameters.nozzle_div_t * 1e3:.3f} mm"
+        )
+        print(
+            f" Minimal bulkhead thickness: {self.structural_parameters.bulkhead_t * 1e3:.3f} mm"
+        )
+        print(
+            f" Optimal number of screws: {self.structural_parameters.optimal_fasteners + 1:d}"
+        )
+        print(
+            f" Shear, tear, compression screw safety factors: "
+            f"{self.structural_parameters.shear_sf[self.structural_parameters.optimal_fasteners]:.3f}, "
+            f"{self.structural_parameters.tear_sf[self.structural_parameters.optimal_fasteners]:.3f}, "
+            f"{self.structural_parameters.compression_sf[self.structural_parameters.optimal_fasteners]:.3f}"
+        )
+        print("\nDISCLAIMER: values above shall not be the final dimensions.")
+        print(
+            "Critical dimensions shall be investigated in depth in order to guarantee safety."
         )
