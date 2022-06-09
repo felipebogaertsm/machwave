30d58321c66fa240d57fe21aef2bd9cad62da13a
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-06-09 19:41:10 -0300

    Monte carlo setup

--- srm_solver/montecarlo/__init__.py
+++ srm_solver/montecarlo/__init__.py
@@ -6,7 +6,7 @@
 # the Free Software Foundation, version 3.
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Any, Optional
 
 import numpy as np
 
@@ -16,7 +16,7 @@ from simulations import Simulation
 @dataclass
 class MonteCarloParameter:
     """
-    Stores a Monte Carlo parameter alongside its uppser/lower bound.
+    Stores a Monte Carlo parameter alongside its upper/lower bound.
     """
 
     value: float | int
@@ -30,6 +30,39 @@ class MonteCarloParameter:
             high=self.value + self.upper_tolerance + self.tolerance,
         )
 
+    def __lt__(self, other: Any) -> bool:
+        return self.value < other
+
+    def __gt__(self, other: Any) -> bool:
+        return self.value > other
+
+    def __ge__(self, other: Any) -> bool:
+        return self.value >= other
+
+    def __le__(self, other: Any) -> bool:
+        return self.value <= other
+
+    def __add__(self, other: Any) -> float:
+        try:
+            return self.value + other.value
+        except AttributeError:
+            return self.value + other
+
+    def __sub__(self, other: Any) -> float:
+        try:
+            return self.value - other.value
+        except AttributeError:
+            return self.value - other
+
+    def __pow__(self, other: Any) -> float:
+        return self.value ** other
+
+    def __truediv__(self, other: Any) -> float:
+        return self.value / other
+
+    def __rmul__(self, other: Any) -> float:
+        return self.value * other
+
 
 class MonteCarloSimulation:
     """
@@ -38,7 +71,7 @@ class MonteCarloSimulation:
 
     def __init__(
         self,
-        parameters: list[MonteCarloParameter],
+        parameters: list[Any],
         number_of_scenarios: int,
         simulation: Simulation,
     ) -> None:
@@ -46,14 +79,37 @@ class MonteCarloSimulation:
         self.number_of_scenarios = number_of_scenarios
         self.simulation = simulation
 
+        self.scenarios: list[list[float | int]] = []
+
     def generate_scenario(self) -> list[float | int]:
-        return [parameter.get_random_value() for parameter in self.parameters]
+        new_scenario = []
+
+        for parameter in self.parameters:
+            if isinstance(parameter, MonteCarloParameter):
+                parameter = parameter.get_random_value()
+            else:
+                try:
+                    for _, sub_param in vars(parameter).items():
+                        print(sub_param)
+                        if isinstance(sub_param, MonteCarloParameter):
+                            print("hey")
+                            sub_param = sub_param.get_random_value()
+                            print("Sub param:", sub_param)
+                            parameter[sub_param] = sub_param
+                except TypeError:
+                    pass
+
+            new_scenario.append(parameter)
+
+        self.scenarios.append(new_scenario)
+        return new_scenario
 
     def run(self) -> None:
         results = []
 
         for _ in range(self.number_of_scenarios):
             scenario = self.generate_scenario()
-            results.append(self.simulation.run(*scenario))
+            results.append(self.simulation(*scenario).run())
+            print("Apogee:", np.max(results[-1][2].y))
 
         return results
