53777198698770da62facc197950606bd6a7c7d8
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-07-11 10:59:54 -0300

    IB simulation

---
+++ srm_solver/utils/classes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Author: Felipe Bogaerts de Mattos
+# Contact me at felipe.bogaerts@engenharia.ufjf.br.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+from models.propulsion import Motor, SolidMotor
+from operations.internal_ballistics import MotorOperation, SRMOperation
+
+
+def get_motor_operation_class(motor: Motor) -> MotorOperation:
+    """
+    Will depend on the type of the motor (SR, HRE or LRE).
+    """
+    if isinstance(motor, SolidMotor):
+        return SRMOperation
