f22c08dbbcaf32fa5b17ff3ec21dda57de834288
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-08-16 09:26:07 -0300

    Removed web

--- web/scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate --noinput
-
-echo "STARTING GUNICORN SERVER..."
-gunicorn config.wsgi:application -t 1800 --bind :8000
