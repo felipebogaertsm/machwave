4a59d23ae07087932ab0710886f493c74b7ddec1
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-08-26 15:56:42 -0300

    Trap fins lift coefficient

--- rocketsolver/models/rocket/fuselage/components/fins/__init__.py
+++ rocketsolver/models/rocket/fuselage/components/fins/__init__.py
@@ -5,6 +5,8 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+import numpy as np
+
 from rocketsolver.models.materials import Material
 from rocketsolver.models.rocket.fuselage.components import FuselageComponent
 from rocketsolver.utils.geometric import get_trapezoidal_area
@@ -29,4 +31,19 @@ class TrapezoidalFins(FuselageComponent):
     def get_area(self):
         return get_trapezoidal_area(
             self.base_length, self.tip_length, self.height
+        )
+
+    def get_AR(self):
+        return ((self.base_length / 2) ** 2) / self.get_area()
+
+    def get_A_1_4(self) -> float:
+        return np.deg2rad(45)
+
+    def get_lift_coefficient(self, *args, **kwargs) -> float:
+        return (2 * np.pi * self.get_AR()) / (
+            2
+            + np.cos(self.get_A_1_4())
+            * np.sqrt(
+                4 + ((self.get_AR() ** 2) / (np.cos(self.get_A_1_4()) ** 4))
+            )
         )
