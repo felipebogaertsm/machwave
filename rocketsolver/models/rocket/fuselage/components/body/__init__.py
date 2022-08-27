e597119300c4bf4dd0bfed9f11f964f75e01e739
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-08-26 23:12:19 -0300

    Finished lift coeff

--- rocketsolver/models/rocket/fuselage/components/body/__init__.py
+++ rocketsolver/models/rocket/fuselage/components/body/__init__.py
@@ -5,6 +5,8 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+import numpy as np
+
 from rocketsolver.models.materials import Material
 from rocketsolver.models.rocket.fuselage.components import FuselageComponent
 
@@ -15,10 +17,26 @@ class CylindricalBody(FuselageComponent):
         material: Material,
         length: float,
         outer_diameter: float,
-        surface_rugosity: float,
+        rugosity: float,
+        constant_K: float,
     ) -> None:
         super().__init__(material)
 
         self.length = length
         self.outer_diameter = outer_diameter
-        self.surface_rugosity = surface_rugosity
+        self.rugosity = rugosity
+        self.constant_K = constant_K
+
+    @property
+    def frontal_area(self) -> float:
+        return np.pi * self.outer_diameter**2 / 4
+
+    @property
+    def outer_surface_area(self) -> float:
+        return self.length * np.pi * (self.outer_diameter)
+
+    def get_drag_coefficient(self) -> float:
+        pass
+
+    def get_lift_coefficient(self) -> float:
+        return self.constant_K * self.outer_surface_area / self.frontal_area
