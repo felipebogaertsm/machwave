89dbc303189ac2c77da400ff7e0189c78bb59054
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-08-29 10:46:34 -0300

    Bug fixes

--- rocketsolver/simulations/ballistics_6dof.py
+++ rocketsolver/simulations/ballistics_6dof.py
@@ -5,11 +5,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 
+from typing import Optional
+
 import numpy as np
 
 from rocketsolver.models.atmosphere import Atmosphere
-from rocketsolver.models.recovery import Recovery
-from rocketsolver.models.fuselage import Fuselage3D
+from rocketsolver.models.rocket import Rocket3D
 from rocketsolver.operations.internal_ballistics import MotorOperation
 from rocketsolver.operations.ballistics._6dof import Ballistic6DOFOperation
 from rocketsolver.simulations import Simulation
@@ -19,31 +20,23 @@ from rocketsolver.utils.classes import get_motor_operation_class
 class Ballistic6DOFSimulation(Simulation):
     def __init__(
         self,
-        thrust: np.ndarray,
-        motor_dry_mass: float,
-        initial_propellant_mass: float,
-        time: np.ndarray,
-        fuselage: Fuselage3D,
-        recovery: Recovery,
+        rocket: Rocket3D,
         atmosphere: Atmosphere,
         d_t: float,
         initial_elevation_amsl: float,
         rail_length: float,
         launch_angle: float,
         heading_angle: float,
+        igniter_pressure: Optional[float] = 1.5e6,
     ) -> None:
-        self.thrust = thrust
-        self.initial_propellant_mass = initial_propellant_mass
-        self.motor_dry_mass = motor_dry_mass
-        self.time = time
-        self.fuselage = fuselage
-        self.recovery = recovery
+        self.rocket = rocket
         self.atmosphere = atmosphere
         self.d_t = d_t
         self.initial_elevation_amsl = initial_elevation_amsl
         self.rail_length = rail_length
         self.launch_angle = launch_angle
         self.heading_angle = heading_angle
+        self.igniter_pressure = igniter_pressure
 
         self.t = np.array([0])
 
@@ -51,10 +44,12 @@ class Ballistic6DOFSimulation(Simulation):
         """
         Will depend on the type of the motor (SR, HRE or LRE).
         """
-        motor_operation_class = get_motor_operation_class(self.motor)
+        motor_operation_class = get_motor_operation_class(
+            self.rocket.propulsion
+        )
 
         return motor_operation_class(
-            motor=self.motor,
+            motor=self.rocket.propulsion,
             initial_pressure=self.igniter_pressure,
             initial_atmospheric_pressure=self.atmosphere.get_pressure(
                 self.initial_elevation_amsl
@@ -64,18 +59,18 @@ class Ballistic6DOFSimulation(Simulation):
     def run(self) -> tuple[np.array, Ballistic6DOFOperation]:
         self.motor_operation = self.get_motor_operation()
         self.ballistic_operation = Ballistic6DOFOperation(
-            fuselage=self.fuselage,
-            recovery=self.recovery,
+            fuselage=self.rocket.fuselage,
+            recovery=self.rocket.recovery,
             atmosphere=self.atmosphere,
             rail_length=self.rail_length,
-            motor_dry_mass=self.motor_dry_mass,
+            motor_dry_mass=self.rocket.propulsion.get_dry_mass(),
             initial_vehicle_mass=60,
         )
 
         i = 0
 
         while (
-            self.ballistic_operation.altitude[i] >= 0
+            self.ballistic_operation.position[i][2] >= 0  # altitude >= 0
             or self.motor_operation.m_prop[-1] > 0
         ):
             self.t = np.append(self.t, self.t[i] + self.d_t)  # new time value
