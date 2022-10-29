ba39fc7a46430a70a46ee3d39cea6317ab7c5f7d
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-10-29 18:36:57 +0200

    Started multiport segment

--- rocketsolver/models/propulsion/grain/geometries/__init__.py
+++ rocketsolver/models/propulsion/grain/geometries/__init__.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+# Author: Felipe Bogaerts de Mattos
+# Contact me at me@felipebm.com.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
 from .bates import BatesSegment
 from .conical import ConicalGrainSegment
 from .d_grain import DGrainSegment
+from .multi_port import MultiPortGrainSegment
 from .rod_and_tube import RodAndTubeGrainSegment
 from .star import StarGrainSegment
 from .wagon_wheel import WagonWheelGrainSegment
