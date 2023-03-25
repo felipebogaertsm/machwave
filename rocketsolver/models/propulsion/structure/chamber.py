Fixed propellant volume calculation

rocketsolver/models/propulsion/structure/chamber.py
@@ -75,18 +75,18 @@ class CombustionChamber:
 
     def get_casing_stress_theta(self, chamber_pressure: float) -> float:
         return (
-            (chamber_pressure * self.casing_inner_radius**2)
-            / (self.outer_radius**2 - self.casing_inner_radius**2)
+            (chamber_pressure * self.casing_inner_radius ** 2)
+            / (self.outer_radius ** 2 - self.casing_inner_radius ** 2)
             * (
                 1
-                + ((self.outer_radius**2) / (self.casing_inner_radius**2))
+                + ((self.outer_radius ** 2) / (self.casing_inner_radius ** 2))
             )
         )
 
     def get_casing_stress_radius(self, chamber_pressure: float) -> float:
         return (
-            (chamber_pressure * self.casing_inner_radius**2)
-            / (self.outer_radius**2 - self.casing_inner_radius**2)
+            (chamber_pressure * self.casing_inner_radius ** 2)
+            / (self.outer_radius ** 2 - self.casing_inner_radius ** 2)
             * (1 - ((self.outer_radius) / (self.casing_inner_radius)) ** 2)
         )
 
@@ -94,8 +94,8 @@ class CombustionChamber:
         return (
             2
             * chamber_pressure
-            * self.casing_inner_radius**2
-            / (self.outer_radius**2 - self.casing_inner_radius**2)
+            * self.casing_inner_radius ** 2
+            / (self.outer_radius ** 2 - self.casing_inner_radius ** 2)
         )
 
     def get_casing_safety_factor(self, chamber_pressure: np.ndarray) -> float:
@@ -151,7 +151,7 @@ class BoltedCombustionChamber(CombustionChamber):
         self.screw_diameter = screw_diameter
 
     def get_shear_area(self) -> float:
-        return (self.screw_diameter**2) * np.pi * 0.25
+        return (self.screw_diameter ** 2) * np.pi * 0.25
 
     def get_tear_area(self, screw_count: int) -> float:
         """
@@ -161,7 +161,7 @@ class BoltedCombustionChamber(CombustionChamber):
             (
                 np.pi
                 * 0.25
-                * ((self.outer_diameter**2) - (self.inner_diameter**2))
+                * ((self.outer_diameter ** 2) - (self.inner_diameter ** 2))
             )
             / screw_count
         ) - (
@@ -169,7 +169,7 @@ class BoltedCombustionChamber(CombustionChamber):
                 (self.screw_clearance_diameter / 2) / (self.inner_diameter / 2)
             )
         ) * 0.25 * (
-            (self.outer_diameter**2) - (self.inner_diameter**2)
+            (self.outer_diameter ** 2) - (self.inner_diameter ** 2)
         )
 
     def get_compression_area(self) -> float: