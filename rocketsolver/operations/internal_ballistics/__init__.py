Fixed grain mass flux calculation

rocketsolver/operations/internal_ballistics/__init__.py
@@ -361,13 +361,10 @@ class SRMOperation(MotorOperation):
 
     @property
     def grain_mass_flux(self) -> np.ndarray:
-        return np.sum(
-            self.motor.grain.get_mass_flux_per_segment(
-                self.burn_rate,
-                self.motor.propellant.density,
-                self.web,
-            ),
-            axis=0,
+        return self.motor.grain.get_mass_flux_per_segment(
+            self.burn_rate,
+            self.motor.propellant.density,
+            self.web,
         )
 
     @property