1561230ea199d50ee3befe8f8f3c840505551361
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-06-17 14:57:05 -0300

    Update thermals.py

--- rocketsolver/models/propulsion/thermals.py
+++ rocketsolver/models/propulsion/thermals.py
@@ -3,5 +3,12 @@ from rocketsolver.models.materials import Material
 
 class ThermalLiner:
     def __init__(self, thickness: float, material: Material) -> None:
+        """
+        Initializes a ThermalLiner object.
+
+        Args:
+            thickness (float): The thickness of the thermal liner, in meters.
+            material (Material): The material used for the thermal liner.
+        """
         self.thickness = thickness
         self.material = material
