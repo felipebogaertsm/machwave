b5084586778f0bca98f2bc9ec53ddf02f2610440
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-06-17 11:35:09 -0300

    Materials as dataclasses

--- rocketsolver/models/materials/composites.py
+++ rocketsolver/models/materials/composites.py
@@ -1,8 +1,25 @@
+from dataclasses import dataclass
+
 from . import Material
 
 
+@dataclass
 class Fiberglass(Material):
-    def __init__(self) -> None:
-        super().__init__(
-            density=1700, yield_strength=200e6, ultimate_strength=210e6
-        )
+    """
+    Fiberglass material class derived from the Material base class.
+
+    This class represents a specific type of material, Fiberglass, which
+    inherits properties from the Material base class. It provides default
+    values for the density, yield strength, and ultimate strength specific to
+    Fiberglass.
+
+    Inherits:
+        Material: Base class representing a generic material.
+
+    Attributes:
+        None
+    """
+
+    density: float = 1700
+    yield_strength: float = 200e6
+    ultimate_strength: float = 210e6
