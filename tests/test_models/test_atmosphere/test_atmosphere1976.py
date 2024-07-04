7266f4192b13ce8e7971e1a1b65791a99201723e
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-07-04 10:44:50 -0300

    Update test_atmosphere1976.py

--- tests/test_models/test_atmosphere/test_atmosphere1976.py
+++ tests/test_models/test_atmosphere/test_atmosphere1976.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
-from rocketsolver.models.atmosphere import Atmosphere, Atmosphere1976
+from rocketsolver.models.atmosphere import Atmosphere
+from rocketsolver.models.atmosphere.atm_1976 import Atmosphere1976
 
 
 def test_atmosphere1976_up_to_karman_line(
