647976a27eee96ced112134290d8cec63a3982bc
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-09-30 22:29:07 -0300

    Added up_to_karman_line test to atmosphere with wind power law

--- tests/test_models/test_atmosphere/test_atmosphere1976.py
+++ tests/test_models/test_atmosphere/test_atmosphere1976.py
@@ -8,3 +8,12 @@ def test_atmosphere1976_up_to_karman_line(
     test_atmosphere_up_to_karman_line: Callable[[Atmosphere], None]
 ) -> None:
     test_atmosphere_up_to_karman_line(atmosphere=Atmosphere1976())
+
+
+def test_atmosphere1976windpowerlaw_up_to_karman_line(
+    test_atmosphere_up_to_karman_line: Callable[[Atmosphere], None],
+    atmosphere1976withwindpowerlaw: Atmosphere1976,
+) -> None:
+    test_atmosphere_up_to_karman_line(
+        atmosphere=atmosphere1976withwindpowerlaw
+    )
