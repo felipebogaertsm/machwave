d538b33d68e7a60cfb3b5d16f82345875aa1587c
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-03-05 10:44:25 -0300

    Refactored geometric

--- machwave/services/math/geometric.py
+++ machwave/services/math/geometric.py
@@ -8,7 +8,7 @@ def get_circle_area(diameter: float) -> float:
     Returns the area of a circle based on the circle's diameter.
 
     Args:
-        diameter (float): The diameter of the circle.
+        diameter: The diameter of the circle.
 
     Returns:
         float: The area of the circle.
@@ -21,8 +21,8 @@ def get_torus_area(major_radius: float, minor_radius: float) -> float:
     Calculates the surface area of a torus.
 
     Args:
-        major_radius (float): The major radius of the torus.
-        minor_radius (float): The minor radius of the torus.
+        major_radius: The major radius of the torus.
+        minor_radius: The minor radius of the torus.
 
     Returns:
         float: The surface area of the torus.
@@ -35,12 +35,12 @@ def get_trapezoidal_area(base_length: float, tip_length: float, height: float) -
     Calculates the area of a trapezoid.
 
     Args:
-        base_length (float): The length of the base of the trapezoid.
-        tip_length (float): The length of the tip of the trapezoid.
-        height (float): The height of the trapezoid.
+        base_length: The length of the base of the trapezoid.
+        tip_length: The length of the tip of the trapezoid.
+        height: The height of the trapezoid.
 
     Returns:
-        float: The area of the trapezoid.
+        The area of the trapezoid.
     """
     return (base_length + tip_length) * height / 2
 
@@ -50,8 +50,8 @@ def get_cylinder_surface_area(length: float, diameter: float) -> float:
     Returns the surface area of a cylinder.
 
     Args:
-        length (float): The length of the cylinder.
-        diameter (float): The diameter of the cylinder.
+        length: The length of the cylinder.
+        diameter: The diameter of the cylinder.
 
     Returns:
         float: The surface area of the cylinder.
@@ -64,27 +64,29 @@ def get_cylinder_volume(diameter: float, length: float) -> float:
     Returns the volume of a cylinder.
 
     Args:
-        diameter (float): The diameter of the cylinder.
-        length (float): The length of the cylinder.
+        diameter: The diameter of the cylinder.
+        length: The length of the cylinder.
 
     Returns:
-        float: The volume of the cylinder.
+        The volume of the cylinder.
     """
     return np.pi * length * (diameter**2) / 4
 
 
-def get_contours(map: np.ndarray, map_dist: float, *args, **kwargs) -> np.ndarray:
+def get_contours(
+    map: np.ndarray, map_dist: float, *args, **kwargs
+) -> list[np.typing.NDArray[np.float64]]:
     """
     Finds contours in an image.
 
     Args:
-        map (np.ndarray): The input image.
-        map_dist (float): The map distance.
+        map: The input image.
+        map_dist: The map distance.
         *args: Additional arguments to be passed to the find_contours function.
         **kwargs: Additional keyword arguments to be passed to the find_contours function.
 
     Returns:
-        np.ndarray: An array of contours.
+        An array of contours.
     """
     return measure.find_contours(map, map_dist, fully_connected="low", *args, **kwargs)
 
@@ -97,12 +99,12 @@ def get_length(
     'tolerance' of the edge of a circle with diameter 'map_size'.
 
     Args:
-        contour (np.ndarray): The contour array.
-        map_size (int): The size of the map.
-        tolerance (float, optional): The tolerance value. Defaults to 3.0.
+        contour: The contour array.
+        map_size: The size of the map.
+        tolerance: The tolerance value. Defaults to 3.0.
 
     Returns:
-        float: The total length of the segments.
+        The total length of the segments.
     """
     offset = np.roll(contour.T, 1, axis=1)
     lengths = np.linalg.norm(contour.T - offset, axis=0)
