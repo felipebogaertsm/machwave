d0ca0fc0a20eecc17fdf5568b4fb25e9e9654382
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-03-18 12:18:42 -0300

    Added liquidpropellant class

---
+++ machwave/models/propulsion/propellants/liquid.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from machwave.models.propulsion.propellants import Propellant
+
+
+@dataclass
+class LiquidPropellant(Propellant):
+    """
+    Stores liquid propellant data.
+
+    TODO: Implement class.
+
+    Inherits:
+        Propellant: Base class representing a propellant.
+    """
