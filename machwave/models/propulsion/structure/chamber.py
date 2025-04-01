0084fa03c1739764e2b5385c1e27f30a79434366
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-04-01 09:48:01 -0300

    Formatting

--- machwave/models/propulsion/structure/chamber.py
+++ machwave/models/propulsion/structure/chamber.py
@@ -153,9 +153,7 @@ class BoltedCombustionChamber(CombustionChamber):
             / screw_count
         ) - (
             np.arcsin((self.screw_clearance_diameter / 2) / (self.inner_diameter / 2))
-        ) * 0.25 * (
-            (self.outer_diameter**2) - (self.inner_diameter**2)
-        )
+        ) * 0.25 * ((self.outer_diameter**2) - (self.inner_diameter**2))
 
     def get_compression_area(self) -> float:
         return (
