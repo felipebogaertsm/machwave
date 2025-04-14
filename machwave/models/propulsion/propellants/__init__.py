4eb1d0a3553a514cd8a10e3445f7366e600d79c4
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-04-13 22:09:25 -0300

    Finished propulsion module for biliquid propellants

--- machwave/models/propulsion/propellants/__init__.py
+++ machwave/models/propulsion/propellants/__init__.py
@@ -1,17 +1,2 @@
-from abc import ABC
-from dataclasses import dataclass
-
-
-@dataclass
-class Propellant(ABC):
-    """
-    Base class for any propellant.
-
-    Inherits:
-        ABC: Abstract Base Class representing an abstract class.
-
-    Attributes:
-        None
-    """
-
-    pass
+from machwave.models.propulsion.propellants.biliquid import BiliquidPropellant
+from machwave.models.propulsion.propellants.solid import SolidPropellant
--- machwave/operations/internal_ballistics/__init__.py
+++ machwave/operations/internal_ballistics/__init__.py
@@ -1,5 +1,7 @@
 from machwave.operations.internal_ballistics.base import MotorOperation
 from machwave.operations.internal_ballistics.solid_motor import SolidMotorOperation
-from machwave.operations.internal_ballistics.liquid_engine import LiquidEngineOperation
+from machwave.operations.internal_ballistics.liquid_engine import (
+    LiquidEngineOperation,
+)
 
 __all__ = ["MotorOperation", "SolidMotorOperation", "LiquidEngineOperation"]
