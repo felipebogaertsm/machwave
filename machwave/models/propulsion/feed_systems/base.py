87790f8bf21ee02e33d44db4a360c5c670869f12
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-04-15 12:11:50 -0300

    Finished base FeedSystem class

--- machwave/models/propulsion/feed_systems/base.py
+++ machwave/models/propulsion/feed_systems/base.py
@@ -1,27 +1,43 @@
 from abc import ABC, abstractmethod
+from machwave.models.propulsion.feed_systems.tanks.base import Tank
 
 
 class FeedSystem(ABC):
     """
-    Abstract base class for a bipropellant feed system in a liquid rocket
-    engine (LRE).
+    Abstract base class for a bipropellant feed system in a liquid rocket engine (LRE).
 
-    This class is responsible for determining oxidizer and fuel mass flows as a
-    function of tank/pressurant states, pumps (if any), and current chamber
-    conditions. Subclasses must implement the abstract methods to specify the
+    This class is responsible for determining oxidizer and fuel mass flows as a function of tank/pressurant states,
+    pumps (if any), and current chamber conditions. Subclasses must implement the abstract methods to specify the
     actual flow calculations.
     """
+
+    def __init__(self, fuel_tank: Tank, oxidizer_tank: Tank):
+        """
+        Initialize the FeedSystem with associated tank objects.
+
+        Args:
+            fuel_tank (Tank): An instance representing the fuel tank.
+            oxidizer_tank (Tank): An instance representing the oxidizer tank.
+        """
+        self.fuel_tank = fuel_tank
+        self.oxidizer_tank = oxidizer_tank
 
     @abstractmethod
     def get_mass_flow_ox(self, *args, **kwargs) -> float:
         """
-        Returns the current oxidizer mass flow rate [kg/s].
+        Compute and return the current oxidizer mass flow rate.
+
+        Returns:
+            float: The oxidizer mass flow rate in kilograms per second (kg/s).
         """
         pass
 
     @abstractmethod
     def get_mass_flow_fuel(self, *args, **kwargs) -> float:
         """
-        Returns the current fuel mass flow rate [kg/s].
+        Compute and return the current fuel mass flow rate.
+
+        Returns:
+            float: The fuel mass flow rate in kilograms per second (kg/s).
         """
         pass
