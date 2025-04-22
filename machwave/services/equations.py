2e61e6a61f4e44aa9c40d374034259797c2f0f1b
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-04-21 22:56:51 -0300

    Fixed equation fot dpo/dt

--- machwave/services/equations.py
+++ machwave/services/equations.py
@@ -65,7 +65,7 @@ def solve_pressure_fed_lre_chamber_pressure(
     m_dot_ox: float,
     m_dot_fuel: float,
 ) -> tuple[float]:
-    dP0_dt = (R * T0 / V0) * ((m_dot_ox + m_dot_fuel) - (P0 * At * c_star))
+    dP0_dt = (R * T0 / V0) * ((m_dot_ox + m_dot_fuel) - (P0 * At / c_star))
     return (dP0_dt,)
 
 
