81f27237615f11fdce008d71f28754e9cabba991
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-04-22 08:41:36 -0300

    Ruff fix

--- machwave/operations/internal_ballistics/liquid_engine.py
+++ machwave/operations/internal_ballistics/liquid_engine.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from machwave.models.propulsion.motors import LiquidEngine
-from machwave.models.propulsion.propellants import BiliquidPropellant
 from machwave.operations.internal_ballistics.base import MotorOperation
 from machwave.services.equations import solve_pressure_fed_lre_chamber_pressure
 from machwave.solvers.odes import rk4th_ode_solver
