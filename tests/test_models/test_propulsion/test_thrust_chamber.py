39d8aba624677ac0af13357ccc5edc932d2d20f5
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-05-15 09:39:18 -0300

    Fixed tests

--- tests/test_models/test_propulsion/test_thrust_chamber.py
+++ tests/test_models/test_propulsion/test_thrust_chamber.py
@@ -1,27 +1,20 @@
-def _test_combustion_chamber_properties(combustion_chamber):
+def _test_combustion_chamber_properties(combustion_chamber_olympus):
     """
     Generic test function for CombustionChamber and its descendents.
 
     Tests geometric properties of the class, such as inner radius
     (calculated from inner diameter) and more.
     """
-    net = combustion_chamber.inner_diameter
-    gross = combustion_chamber.casing_inner_diameter
+    net = combustion_chamber_olympus.inner_diameter
+    gross = combustion_chamber_olympus.casing_inner_diameter
 
     assert net > 0
-    assert net == gross - 2 * combustion_chamber.liner.thickness
+    assert net == gross - 2 * combustion_chamber_olympus.thermal_liner_thickness
 
-    assert combustion_chamber.inner_radius == net / 2
-    assert combustion_chamber.outer_radius == combustion_chamber.outer_diameter / 2
+    assert combustion_chamber_olympus.inner_radius == net / 2
+    assert (
+        combustion_chamber_olympus.outer_radius
+        == combustion_chamber_olympus.outer_diameter / 2
+    )
 
-    assert gross == net + 2 * combustion_chamber.liner.thickness
-
-
-def test_combustion_chamber_properties(combustion_chamber_olympus):
-    _test_combustion_chamber_properties(combustion_chamber_olympus)
-
-
-def test_bolted_combustion_chamber_properties(
-    bolted_combustion_chamber_olympus,
-):
-    _test_combustion_chamber_properties(bolted_combustion_chamber_olympus)
+    assert gross == net + 2 * combustion_chamber_olympus.liner.thickness
