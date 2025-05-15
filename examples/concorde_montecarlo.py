52cb550e0415cea5251c98af739841bdde1c1a38
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-05-15 13:59:22 -0300

    Refactored examples

--- examples/concorde_montecarlo.py
+++ examples/concorde_montecarlo.py
@@ -67,7 +67,7 @@ def main():
         internal_length=grain.total_length + 0.01,
     )
 
-    thrust_chamber = thrust_chamber_models.ThrustChamber(
+    thrust_chamber = thrust_chamber_models.SolidMotorThrustChamber(
         dry_mass=21.013,
         nozzle=nozzle,
         combustion_chamber=combustion_chamber,
