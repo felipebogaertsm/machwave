7e6e1e62753cd740dffabfedadc798bf76a4a719
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-05-19 19:23:13 -0300

    New kinetics correction factor function

--- machwave/core/correction_factors.py
+++ machwave/core/correction_factors.py
@@ -31,3 +31,37 @@ def get_nozzle_divergent_correction_factor(divergent_angle: float) -> float:
         correction_factor = get_divergent_correction_factor(15.0)
     """
     return 0.5 * (1 - np.cos(np.deg2rad(divergent_angle)))
+
+
+def get_kinetics_correction_factor(
+    i_sp_th_frozen: float, i_sp_th_shifting: float, chamber_pressure: float
+) -> float:
+    """
+    The kinetics correction factor accounts for the decrement in
+    performance due to incomplete heat transfer of latent heat to
+    sensible heat caused by the finite time required for the
+    gas phase chemical reactions to occur.
+
+    Valid for liquid, solid, and hybrid propellants.
+    The expansion ratio of the i_sp_th_frozen and i_sp_th_shifting
+    should be the same.
+
+    Pressure correction is applied for chamber pressures
+    above 1.379 MPa (200 psi), in order to dampen the effect of
+    the kinetics correction factor.
+
+    Args:
+        i_sp_th_frozen (float): The specific impulse of the frozen flow.
+        i_sp_th_shifting (float): The specific impulse of the shifting flow.
+        chamber_pressure (float): The chamber pressure in Pascals.
+    Returns:
+        float: The kinetics correction factor.
+    """
+    i_sp_th_ratio = i_sp_th_frozen / i_sp_th_shifting
+
+    if chamber_pressure < 1.379e6:
+        pressure_correction = 1.0
+    else:
+        pressure_correction = 1.379e-6 / chamber_pressure
+
+    return 33.3 / 100 * (1 - i_sp_th_ratio) * pressure_correction
