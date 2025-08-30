d332d06b0297148198002cb3cd9b9000d353995f
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-08-30 03:15:44 -0300

    Created PropellantProperties class

--- machwave/models/propulsion/propellants/base.py
+++ machwave/models/propulsion/propellants/base.py
@@ -1,4 +1,19 @@
 import abc
+import dataclasses
+
+
+import rocketcea.cea_obj as cea
+
+
+@dataclasses.dataclass
+class PropellantProperties:
+    gamma_chamber: float
+    gamma_exhaust: float
+    adiabatic_flame_temperature: float
+    molecular_weight_chamber: float
+    molecular_weight_exhaust: float
+    i_sp_frozen: float
+    i_sp_shifting: float
 
 
 class Propellant(abc.ABC):
@@ -6,18 +21,8 @@ class Propellant(abc.ABC):
     Base class for propellants. To update the propellant parameters, use the
     `evaluate` method.
 
-    Still need to define how to compose different propellant with
+    TODO: Still need to define how to compose different propellant with
     different mixture ratios, solid and liquid.
-
-    Attributes:
-        combustion_efficiency: Combustion efficiency (0 to 1).
-        gamma_chamber: Isentropic exponent for the combustion chamber.
-        gamma_exhaust: Isentropic exponent for the exhaust.
-        adiabatic_flame_temperature: Adiabatic flame temperature [K].
-        molecular_weight_chamber: Molar weight in the chamber [kg/mol].
-        molecular_weight_exhaust: Molar weight in the exhaust [kg/mol].
-        specific_impulse_frozen: Frozen specific impulse [s].
-        specific_impulse_shifting: Shifting specific impulse [s].
     """
 
     def __init__(self, combustion_efficiency: float = 0.95):
@@ -26,15 +31,16 @@ class Propellant(abc.ABC):
         self._initial_pressure = 1000  # psi
         self._initial_expansion_ratio = 8
 
+        # CEA object to be defined
+        self.cea_obj = None
+
         self.evaluate()
 
-    def evaluate(self, *args, **kwargs) -> None:
-        """Calculate propellant properties given the current state."""
+    def evaluate(self, *args, **kwargs) -> PropellantProperties:
+        """
+        Calculate propellant properties given the current state.
 
-        self.gamma_chamber = 0.0
-        self.gamma_exhaust = 0.0
-        self.adiabatic_flame_temperature = 0.0
-        self.molecular_weight_chamber = 0.0
-        self.molecular_weight_exhaust = 0.0
-        self.specific_impulse_frozen = 0.0
-        self.specific_impulse_shifting = 0.0
+        Returns:
+            PropellantProperties: The calculated propellant properties.
+        """
+        self.cea_obj = cea.CEA_Obj(oxName="LOX", fuelName="RP-1")
