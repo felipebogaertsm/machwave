27cb4671f500cff31da3e2b72d84581c5d9d638b
Felipe Bogaerts de Mattos <me@felipebm.com>
2025-08-30 03:15:28 -0300

    Added porosity

--- machwave/models/propulsion/propellants/solid.py
+++ machwave/models/propulsion/propellants/solid.py
@@ -24,28 +24,31 @@ class SolidPropellant(Propellant):
     Extends the Propellant abstract class to represent a solid propellant.
 
     Attributes:
-        burn_rate: List of dictionaries describing
-            burn rate behavior (St. Robert's law parameters) with keys:
-            "min", "max", "a", and "n".
-        condensed_phase_moles_chamber: Number of condensed-phase moles per
-            100 g in the chamber.
-        condensed_phase_moles_exhaust: Number of condensed-phase moles per
-            100 g in the exhaust.
+        burn_rate: List of dictionaries describing burn rate behavior (St.
+            Robert's law parameters) with keys: "min", "max", "a", and "n".
+        qsi_chamber: Number of condensed-phase moles per 100 g in the chamber.
+        qsi_exhaust: Number of condensed-phase moles per 100 g in the exhaust.
+        porosity: Porosity of the solid propellant (0 to 1). Represents the
+            volume fraction of voids in the propellant.
         solid_density: Solid propellant density [kg/m^3].
     """
 
     def __init__(
-        self, burn_rate: list[dict[str, float | int]], combustion_efficiency=0.95
+        self,
+        burn_rate: list[dict[str, float | int]],
+        porosity: float = 0.98,
+        combustion_efficiency: float = 0.95,
     ):
         super().__init__(combustion_efficiency)
+        self.porosity = porosity
         self.burn_rate = burn_rate
 
     def evaluate(self, *args, **kwargs) -> None:
         super().evaluate(*args, **kwargs)
 
-        self.condensed_phase_moles_chamber = 0.0
-        self.condensed_phase_moles_exhaust = 0.0
-        self.solid_density = 0.0
+        self.qsi_chamber = 0.0
+        self.qsi_exhaust = 0.0
+        self.solid_density = 0.0 * self.porosity
 
     def get_burn_rate(self, chamber_pressure: float) -> float:
         """
