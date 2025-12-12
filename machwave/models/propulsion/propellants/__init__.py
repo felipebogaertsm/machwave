74ec5131ab3b6afb4a2006b6d835816a5c4a3034
Felipe Bogaerts de Mattos <felipe.demattos@ledgebrook.com>
2025-12-12 20:32:35 -0300

    Added docstring to propellant module

--- machwave/models/propulsion/propellants/__init__.py
+++ machwave/models/propulsion/propellants/__init__.py
@@ -1,9 +1,18 @@
-from machwave.models.propulsion.propellants.properties import (
-    ChemicalPropellantProperties,
-    LiquidPropellantProperties,
-    SolidPropellantProperties,
-)
-from machwave.models.propulsion.propellants.types import (
+"""
+The models.propulsion.propellants module contains classes that allow the user to
+represent a chemical propellant. There are three main components in it, from least
+to most primitive:
+
+- Properties: classes that contain different propellant thermochemical properties.
+These do not contain operational attributes, such as chamber pressure or instant
+burn rate, but rather contain propellant attributes from those states.
+- Categories: classes that represent different physical states of chemical propellants.
+They can be solid, biliquid, monoliquid, or hybrid.
+- Formulations: ready-made objects that represent a propellant combination. For example
+the solid propellant KNSB, or the liquid propellants LH2/LOX.
+"""
+
+from machwave.models.propulsion.propellants.categories import (
     BiliquidPropellant,
     BurnRateOutOfBoundsError,
     CEASolidPropellant,
@@ -11,16 +20,21 @@ from machwave.models.propulsion.propellants.types import (
     Propellant,
     SolidPropellant,
 )
+from machwave.models.propulsion.propellants.properties import (
+    ChemicalPropellantProperties,
+    LiquidPropellantProperties,
+    SolidPropellantProperties,
+)
 
 __all__ = [
+    # Base classes
+    "Propellant",
+    "BurnRateOutOfBoundsError",
     # Properties
     "ChemicalPropellantProperties",
     "SolidPropellantProperties",
     "LiquidPropellantProperties",
-    # Base classes
-    "Propellant",
-    "BurnRateOutOfBoundsError",
-    # Type classes
+    # Categories
     "BiliquidPropellant",
     "SolidPropellant",
     "FixedSolidPropellant",
