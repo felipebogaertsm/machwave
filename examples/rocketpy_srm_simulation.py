a96deb0f76ee9caa577c4a38abbe70de61faac66
Felipe Bogaerts de Mattos <felipe.demattos@ledgebrook.com>
2026-02-11 09:05:27 -0300

    Started rocketpy srm adapter example

---
+++ examples/rocketpy_srm_simulation.py
@@ -0,0 +1,17 @@
+"""
+Example of a RocketPy 6DOF simulation of a `machwave` solid motor using the
+`RocketPySolidMotorAdapter`.
+"""
+
+from machwave import simulations, states
+from machwave.adapters import rocketpy
+from machwave.common import decorators
+from machwave.models.propulsion import motors
+
+
+@decorators.timing
+def main(): ...
+
+
+if __name__ == "__main__":
+    main()
