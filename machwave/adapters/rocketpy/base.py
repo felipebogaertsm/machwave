0fbcfc8a4a8c9e8afed39286d809433e6a84740e
Felipe Bogaerts de Mattos <felipe.demattos@ledgebrook.com>
2026-02-11 09:05:11 -0300

    Update base.py

--- machwave/adapters/rocketpy/base.py
+++ machwave/adapters/rocketpy/base.py
@@ -3,6 +3,8 @@
 import abc
 import typing
 
+from machwave.core.solvers import interpolation
+
 if typing.TYPE_CHECKING:
     import rocketpy.motors as rocketpy_motors
 
@@ -15,17 +17,45 @@ class RocketPyAdapterError(Exception):
     pass
 
 
-M = typing.TypeVar("M", bound=ib_states.MotorState)
-R = typing.TypeVar("R", bound=rocketpy_motors.Motor)
+M = typing.TypeVar("M", bound=ib_states.MotorState)  # Machwave motor state
+R = typing.TypeVar("R", bound=rocketpy_motors.Motor)  # RocketPy motor
 
 
 class RocketPyMotorAdapter(abc.ABC, typing.Generic[M, R]):
-    """Abstract base class for RocketPy motor adapters.
-
-    Args:
-        motor_state: MotorState from internal ballistics simulation.
-    """
+    """Abstract base class for RocketPy motor adapters."""
 
     def __init__(self, motor_state: M) -> None:
+        """Initialize the adapter with a Machwave motor state.
+
+        Args:
+            motor_state: The Machwave motor state to adapt.
+        """
         self.motor_state = motor_state
         self.motor = motor_state.motor
+
+    def _get_time_series(self) -> dict[str, typing.Any]:
+        """Extract time series data from motor state."""
+        time = self.motor_state.t
+        thrust = self.motor_state.thrust
+
+        def thrust_interpolation(new_time: float) -> float:
+            return interpolation.interpolate_with_time(time, thrust, new_time)  # type: ignore
+
+        return {
+            "thrust": thrust_interpolation,
+            "propellant_mass": self.motor_state.m_prop,
+            # TODO: add other time series
+        }
+
+    def _get_motor_attributes(self) -> dict[str, typing.Any]:
+        """Extract motor attributes from motor model class."""
+        return {
+            "initial_mass": self.motor.get_launch_mass(),
+            "dry_mass": self.motor.get_dry_mass(),
+            # TODO: add all base class attributes
+        }
+
+    @abc.abstractmethod
+    def to_rocketpy_motor(self) -> R:
+        """Convert the machwave motor state to a RocketPy motor object."""
+        ...
