be3642637b29f0ae7e745f0d6b431e85caa4cfe9
Felipe Bogaerts de Mattos <felipe.demattos@ledgebrook.com>
2026-02-17 00:11:04 -0300

    Implemented solid adapter

--- machwave/adapters/rocketpy/solid_motor.py
+++ machwave/adapters/rocketpy/solid_motor.py
@@ -1,18 +1,14 @@
-"""Solid motor adapter for RocketPy."""
+"""RocketPy adapter for Machwave solid motors."""
 
 import typing
 
-from . import base as rocketpy_base
+from machwave.adapters.rocketpy.base import RocketPyMotorAdapter
 
 if typing.TYPE_CHECKING:
-    import rocketpy.motors as rocketpy_motors
-
-    import machwave.states.internal_ballistics.solid_motor as solid_motor_state
+    from machwave.states.internal_ballistics.solid_motor import SolidMotorState
 
 
-class RocketPySolidMotorAdapter(
-    rocketpy_base.RocketPyMotorAdapter[
-        solid_motor_state.SolidMotorState, rocketpy_motors.SolidMotor
-    ]
-):
-    """Adapter to convert machwave SolidMotorState to RocketPy SolidMotor."""
+class RocketPySolidMotorAdapter(RocketPyMotorAdapter["SolidMotorState"]):
+    """Adapter to use Machwave SolidMotorState as a RocketPy SolidMotor."""
+
+    _rocketpy_motor_class = "SolidMotor"
