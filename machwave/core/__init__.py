65a5e6fa638338ae93965796cd929c7c15a1b14b
Felipe Bogaerts de Mattos <felipe.demattos@ledgebrook.com>
2026-02-23 20:53:04 -0300

    Update __init__.py

---
+++ machwave/core/__init__.py
@@ -0,0 +1,5 @@
+"""
+The `machwave.core` module contains functions related to fluid dynamics, geometry,
+numerical methods and other core functionalities used across Machwave. These functions
+are almost immutable, since they are based on fundamental physics and mathematics.
+"""
