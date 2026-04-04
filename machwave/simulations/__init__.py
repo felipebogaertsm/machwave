932ce4d4cca3dc207253e39e1b35235adc58972c
Felipe Bogaerts de Mattos <me@felipebm.com>
2026-04-04 00:08:04 -0300

    Removed ballistics simulations and states

--- machwave/simulations/__init__.py
+++ machwave/simulations/__init__.py
@@ -1,12 +1,4 @@
-from machwave.simulations.ballistics import (
-    BallisticSimulation,
-    BallisticSimulationParameters,
-)
 from machwave.simulations.base import Simulation, SimulationParameters
-from machwave.simulations.internal_balistics_coupled import (
-    InternalBallisticsCoupled,
-    InternalBallisticsCoupledParams,
-)
 from machwave.simulations.internal_ballistics import (
     InternalBallistics,
     InternalBallisticsParams,
@@ -17,8 +9,4 @@ __all__ = [
     "SimulationParameters",
     "InternalBallistics",
     "InternalBallisticsParams",
-    "InternalBallisticsCoupled",
-    "InternalBallisticsCoupledParams",
-    "BallisticSimulation",
-    "BallisticSimulationParameters",
 ]
