7fdca310f78b72346e4990474a834d2baed04035
Felipe Bogaerts de Mattos <me@felipebm.com>
2026-05-26 12:33:26 -0300

    Record free_chamber_volume_rate as a SolidMotorState array
    Computes the term into a local + appends to a new state array (matching
    the existing burn_area / burn_rate / free_chamber_volume pattern) and
    exposes it on SolidSimulationResult.

--- machwave/simulation/solid/states.py
+++ machwave/simulation/solid/states.py
@@ -69,6 +69,7 @@ class SolidMotorState(simulation_states.MotorState):
             self.motor.grain.get_propellant_volume(0.0)
         ]
         self.burn_rate: simulation_states.SimulationStateArray = [0.0]
+        self.free_chamber_volume_rate: simulation_states.SimulationStateArray = [0.0]
 
         initial_cog = motor.grain.get_center_of_gravity(
             web_distance=0.0,
@@ -126,6 +127,8 @@ class SolidMotorState(simulation_states.MotorState):
 
         free_chamber_volume = self.motor.get_free_chamber_volume(propellant_volume)
         self.free_chamber_volume.append(free_chamber_volume)
+        free_chamber_volume_rate = burn_rate * burn_area
+        self.free_chamber_volume_rate.append(free_chamber_volume_rate)
         propellant_mass = self.motor.grain.get_propellant_mass(
             web_distance=web_distance, ideal_density=ideal_propellant_density
         )
@@ -152,7 +155,7 @@ class SolidMotorState(simulation_states.MotorState):
             R=propellant_properties.R_chamber,
             flame_temperature=propellant_properties.adiabatic_flame_temperature,
             nozzle_discharge_coefficient=nozzle.discharge_coefficient,
-            free_chamber_volume_rate=burn_rate * burn_area,
+            free_chamber_volume_rate=free_chamber_volume_rate,
         )[0]
         self.chamber_pressure.append(chamber_pressure)
         exit_pressure = isentropic.get_exit_pressure(
